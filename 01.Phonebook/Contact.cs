04c06bf93253eaa1a04065653960cf3b9910eb57
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2022-11-06 19:48:03 +0200

    Create Contact.cs

---
+++ 01.Phonebook/Contact.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phonebook
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Num { get; set; }
+    }
+}
