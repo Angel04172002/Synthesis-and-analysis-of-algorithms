41b53b8a24706c1248f34edaa5206d423b701139
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-01-21 02:26:13 +0200

    Create Node.cs

---
+++ Course Work/Node.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTMLCrawler
+{
+    public class Node
+    {
+        private string firstTag;
+        private string lastTag;
+        private List<Node> allData;
+        private bool isReadingAll;
+        private List<Node> children;
+
+        public Node(string firstTag, string lastTag, List<Node> allData, bool isReadingAll, string text)
+        {
+            this.firstTag = firstTag;
+            this.lastTag = lastTag;
+            this.allData = allData;
+            this.isReadingAll = isReadingAll;
+            this.Text = text;
+            children = new List<Node>();
+        }
+
+        public string FirstTag { get { return this.firstTag; } }
+        public string LastTag { get { return this.lastTag; } }  
+
+        public string Text { get; set; }
+
+        public bool IsReadingAll { get { return this.isReadingAll; } }  
+
+        public List<Node> Children { get { return this.children; } }
+
+        public string Id { get; set; }
+    
+    }
+}
